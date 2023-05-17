cb39e0d745baa125ee63919bc84149e2593617db
EC2 Default User <ec2-user@ip-172-31-8-173.ap-northeast-1.compute.internal>
2023-05-17 10:03:49 +0000

    投稿の保存

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,19 @@ class PostsController < ApplicationController
   end
 
   def store
-    redirect_to index_post_path
+    @post = Post.new(post_params)
+    
+    if @post.save
+      flash[:notice] = '登録しました'
+      redirect_to index_post_path
+    else
+      render :add
+    end
   end
+  
+  private
+  def post_params
+    params.require(:post).permit(:title, :body, :image)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_16_170038) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_17_073642) do
+  create_table "posts", charset: "latin1", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.string "image_path"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "latin1", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
